9bc387b0ef8316bf4de7b895e2125416553274a7
shilpi <shilpi8624@gmail.com>
2025-12-20 14:05:56 +0530

    :lock: implement bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt');
+
+
 
 const signup = async(req,res)=>{
     const {username, email, password}= req.body;
@@ -8,14 +11,24 @@ const signup = async(req,res)=>{
         return res.status(404).json({ message: "Please enter valid input" });
         }
         
-        const isExistUser = await User.findOne({ where: { email } });
-        if (isExistUser) {
+        const user = await User.findOne({ where: { email } });
+        if (user) {
         return res.status(404).json({ message: "User already exists" });
         }
+        
+        
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, async(err,hash)=>{
+            try{
+                const user = await User.create({username,email,password:hash});
+        
+                res.status(201).json({message:"user signup successfully!"});
+            }catch(err){
+                res.status(500).json({message:"Something went wrong!"});
+            }
+        })
 
-        const user = await User.create({username,email,password});
-        //console.log(user);
-        res.status(201).json({message:"user signup successfully!"});
+        
     }catch(err){
         console.log(err);
         res.status(500).json({message:"Something went wrong!"});
@@ -34,16 +47,30 @@ const login = async(req,res)=>{
         return res.status(401).json({ message: "Please enter valid input" });
         }
         
-        const isExistUser = await User.findOne({ where: { email } });
-        if (!isExistUser) {
+        const user = await User.findOne({ where: { email } });
+        
+        if (!user) {
         return res.status(404).json({ message: "User does not exists" });
         }
+        
+        bcrypt.compare(password, user.password, async(err,result)=>{
+        try{
+            if(err){
+                return res.status(401).json({message:"something went wrong"});
+            }
 
-        if(isExistUser.password !== password){
-            return res.status(401).json({ message: "Invalid credencial" });
+            if(result === true){
+                res.status(201).json({message:"user login successfully!"});
+            }else{
+                return res.status(401).json({message:"password incorrect!"});
+            }
+            
+        }catch(err){
+            res.status(401).json({ message: "Invalid credencial" });
         }
+        })
 
-        res.status(201).json({message:"user login successfully!"});
+        
     }catch(err){
         console.log(err);
         res.status(500).json({message:"Something went wrong!"});
